214d4841ec1c15460b2ccf7f1fc8184a5c8de718
Aakarshak1-1 <shotriaakarshak@gmail.com>
2023-08-13 18:09:14 +0530

    Connections are made

---
+++ db.js
@@ -0,0 +1,11 @@
+// Connecting to the MongoDb
+const mongoose = require("mongoose");
+const mongoose_url = "mongodb://localhost:27017/";
+
+let connected_message = () => {
+  console.log("Connected to the MongoDb");
+};
+const connect_mongo = () => {
+  mongoose.connect(mongoose_url, connected_message());
+};
+module.exports = connect_mongo;
---
+++ index.js
@@ -0,0 +1,13 @@
+const connect_mongo = require("./db");
+connect_mongo();
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
