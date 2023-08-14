3874f69aca25909a6a1aa1edf78f3a3dda428d6e
Aakarshak1-1 <shotriaakarshak@gmail.com>
2023-08-14 11:40:24 +0530

    Refactored code with try catch and validator

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const connect_mongo = require("./db");
 connect_mongo();
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.use(express.json());
 
--- routes/auth.js
+++ routes/auth.js
@@ -11,23 +11,33 @@ router.post(
     body("email", "Enter a valid email").isEmail(),
     body("password", "Enter a valid length password").isLength({ min: 5 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
-
+    // Erros to find
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    console.log(req.body);
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    })
-      .then((user) => res.json(user))
-      .catch((err) => {
-        console.log(err);
-        res.json({ error: "Enter a unique value", message: err.message });
+    // Check if the user exits
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res.status(400).json({
+          errors:
+            "A user with the same email exists so please Enter the unique value",
+        });
+      }
+      // Insert to the Database
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
       });
+      // Response
+      res.json(user);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Some error has been occured on the server");
+    }
   }
 );
 module.exports = router;
