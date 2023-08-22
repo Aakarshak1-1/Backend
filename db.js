d82fc2163cb133491b826198977356b5de530292
Aakarshak1-1 <shotriaakarshak@gmail.com>
2023-08-23 00:10:40 +0530

    Update cloud mongodb

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 // Connecting to the MongoDb
 const mongoose = require("mongoose");
-const mongoose_url = "mongodb://localhost:27017/iNotebook";
-
+// const mongoose_url = "mongodb://localhost:27017/iNotebook";
+const mongoose_url =
+  "mongodb+srv://shotriaakarshak:Ekta%4012%2F05%2F1975@cluster0.si97nbd.mongodb.net/";
 let connected_message = () => {
   console.log("Connected to the MongoDb");
 };
